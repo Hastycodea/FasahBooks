bb6e7eac4dc46cadc934c1577d4bf3db1f48e684
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-31 14:45:57 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,63 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXTwitter, faWhatsapp  } from '@fortawesome/free-brands-svg-icons'
-import { faClock, faPhone } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+  faClock,
+  faChevronDown,
+  faPhone,
+  faBars,
+  faMagnifyingGlass,
+  faUser,
+  faHeart,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className='flex justify-between items-center text-sm text-gray-600'>
-        <div className='flex space-x-3 items-center'>
+    <div className="px-5 ">
+      <div className="flex justify-between items-center text-sm text-gray-600 border-b border-gray-200 py-3">
+        <div className="flex space-x-3 items-center">
           <FontAwesomeIcon icon={faXTwitter} />
           <FontAwesomeIcon icon={faWhatsapp} />
           <FontAwesomeIcon icon={faClock} />
           <p>Mon - Fri: 9am - 5pm | Sat 9am - 3pm</p>
         </div>
-        <div className='flex space-x-2 items-center'>
+        <div className="flex space-x-2 items-center">
           <FontAwesomeIcon icon={faPhone} />
           <p>+254 710 594179</p>
         </div>
       </div>
+      <div className="flex items-center justify-between py-3">
+        <div className="flex items-center">
+          <img src={logo} alt="" className="w-24 h-auto" />
+          <div className="flex mx-5 pb-3  justify-between items-center border-b border-gray-200">
+            <input type="text" placeholder="Product Search" className="p-1.5 outline-none" />
+            <FontAwesomeIcon className="text-2xl" icon={faMagnifyingGlass} />
+          </div>
+        </div>
+        <div className="flex space-x-3 text-2xl ">
+          <FontAwesomeIcon icon={faUser} />
+          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faCartShopping} />
+        </div>
+      </div>
+      <div className="flex  items-center border-b text-sm">
+        <div className="flex space-x-3 items-center p-3 bg-black text-white">
+          <FontAwesomeIcon icon={faBars} />
+          <p>Browse Categories</p>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+        <ul className="flex space-x-10 justify-center text-sm font-medium w-9/12">
+          <li>Home</li>
+          <li>All Books</li>
+          <li>Gift Card</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
