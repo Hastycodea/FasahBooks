4fe614b140786dad5d896dc7cb0c5872ff8583fe
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-11-01 09:09:12 +0300

    sliderUpdated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import hero1 from "../assets/heroimg.png";
-import hero2 from "../assets/egg.png";
-import { Navigation, Autoplay, Pagination, Scrollbar, A11y } from "swiper/modules";
+import hero2 from "../assets/blue.png";
+import {
+  Navigation,
+  Autoplay,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -24,13 +30,16 @@ const Hero = () => {
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
           <div className="flex items-center justify-center bg-[#FF6D1F] h-[500px] my-5 w-full">
-            <img src={hero1} alt="" className="w-5/12 h-full object-cover" />
+            <img
+              src={hero1}
+              alt=""
+              className="w-5/12 h-auto object-cover p-4"
+            />
             <section className="w-6/12 flex flex-col justify-center text-center items-center ">
               <h1 className="text-7xl font-serif font-bold">Now Available</h1>
               <button className="text-2xl bg-black text-[#CD7F32] rounded-lg px-6 py-2 mt-4">
@@ -40,13 +49,17 @@ const Hero = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex items-center justify-center bg-[#FF6D1F] h-[500px] my-5 w-full">
-            <img src={hero2} alt="" className="w-5/12 h-full object-cover" />
-            <section className="w-6/12 flex flex-col justify-center text-center items-center ">
-              <h1 className="text-7xl font-serif font-bold">Now Available</h1>
-              <button className="text-2xl bg-black text-[#CD7F32] rounded-lg px-6 py-2 mt-4">
-                Buy Now
-              </button>
+          <div className="flex items-center justify-center bg-sky-600 h-[500px] my-5 w-full">
+            <img
+              src={hero2}
+              alt=""
+              className="w-5/12 h-auto object-cover p-4"
+            />
+            <section className="w-6/12 flex flex-col text-white justify-center text-center items-center ">
+              <p className="text-2xl font-serif mb-3 ">“Walt Brown beautifully uncovers the invisible layers of life that we often ignore. His reflections encourage leaders to see beyond what is obvious and to listen more deeply.”</p>
+              <p className="text-2xl">~Simon Sinek</p>
+              <p>Author of</p>
+              <p className="italic">Start with Why</p>
             </section>
           </div>
         </SwiperSlide>
