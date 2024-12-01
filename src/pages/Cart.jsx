bb71fa3e3ccc56d84483f15a77e2cc6d5f65ed31
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-01 18:15:53 +0300

    cartPage

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Cart = () => {
-  return (
-    <div>Cart Page to be added</div>
-  )
-}
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
 
-export default Cart
+  return (
+    <div className="w-[90%] mx-auto">
+      <h2 className="text-3xl my-8">Cart</h2>
+      <div className="flex">
+        {cartItems.length === 0 ? (
+          <p>Your Cart is Empty</p>
+        ) : (
+          <>
+            {cartItems.map((item) =>(
+              <p>{item.name}</p>
+
+            ) )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
